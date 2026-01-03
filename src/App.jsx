5ff186d1297e399a0d2aa4fbac2ca7fd3363cc63
Kesarrt <tandulkarkhushi@gmail.com>
2026-01-03 21:06:07 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import {Rouutes,Route} from"react-router-dom"
-import Login from"./pages/Login";
-import AdminDashboard from"./pages/AdminDashboard";
-import AdminDashboard from"./pages/AdminDashboard";
-import CustDash from"./pages/CustomerDashboard";
-import Edit from"./pages/EditRestaurant";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import CustDash from "./pages/CustomerDashboard";
+import EditRestaurant from "./pages/EditRestaurant";
 import PrivateRoute from "./routes/PrivateRoute";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <Routes>
-      <Route path="/"element={<Login />} />
-      <Route path="/admin/
-      dashboard" element={<PrivateRoute roleRequired="Admin"><AdminDashboard/></PrivateRoute>} />
-      <Route path="/admin/restaurants/update/:id"element={<PrivateRoute roleRequired="Admin"><Edit/></PrivateRoute>} />
-      <Route path="/customers/dashboard"element={<PrivateRoute roleRequired="Customer"><CustDash/></PrivateRoute>}/>
+      <Route path="/" element={<Login />} />
+      <Route 
+        path="/admin/dashboard" 
+        element={
+          <PrivateRoute roleRequired="Admin">
+            <AdminDashboard />
+          </PrivateRoute>
+        } 
+      />
+      <Route 
+        path="/admin/restaurants/update/:id" 
+        element={
+          <PrivateRoute roleRequired="Admin">
+            <EditRestaurant />
+          </PrivateRoute>
+        } 
+      />
+      <Route 
+        path="/customers/dashboard" 
+        element={
+          <PrivateRoute roleRequired="Customer">
+            <CustDash />
+          </PrivateRoute>
+        } 
+      />
     </Routes>
   );
 }
